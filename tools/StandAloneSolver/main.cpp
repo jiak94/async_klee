65f00f3e9bf58a3382e091bb31a81b9fe58a4010
Jiakuan Li <jiakuan.li.cs@gmail.com>
2018-03-18 20:19:11 -0400

    wrap up the stand alone solver.

---
+++ tools/StandAloneSolver/main.cpp
@@ -0,0 +1,241 @@
+#include "expr/Lexer.h"
+#include "expr/Parser.h"
+
+#include "klee/Config/Version.h"
+#include "klee/Constraints.h"
+#include "klee/Expr.h"
+#include "klee/ExprBuilder.h"
+#include "klee/Solver.h"
+#include "klee/SolverImpl.h"
+#include "klee/Statistics.h"
+#include "klee/CommandLine.h"
+#include "klee/Common.h"
+#include "klee/util/ExprPPrinter.h"
+#include "klee/util/ExprVisitor.h"
+#include "klee/util/ExprSMTLIBPrinter.h"
+#include "klee/Internal/Support/PrintVersion.h"
+
+#include "llvm/ADT/StringExtras.h"
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/ManagedStatic.h"
+#include "llvm/Support/MemoryBuffer.h"
+#include "llvm/Support/raw_ostream.h"
+
+#include <sys/stat.h>
+#include <unistd.h>
+#include <sys/inotify.h>
+#include <iostream>
+#include <fstream>
+#include <csignal>
+
+
+#include "llvm/Support/Signals.h"
+#include "../../lib/Core/TimingSolver.h"
+
+#if LLVM_VERSION_CODE < LLVM_VERSION(3, 5)
+#include "llvm/ADT/OwningPtr.h"
+#include "llvm/Support/system_error.h"
+#endif
+#define EVENT_SIZE  ( sizeof (struct inotify_event) )
+#define EVENT_BUF_LEN     ( 1024 * ( EVENT_SIZE + 16 ) )
+
+//
+// Created by jiakuan on 3/13/18.
+//
+using namespace llvm;
+using namespace klee;
+using namespace klee::expr;
+
+enum BuilderKinds {
+    DefaultBuilder,
+    ConstantFoldingBuilder,
+    SimplifyingBuilder
+};
+
+llvm::cl::opt<std::string>
+        InputFile2(llvm::cl::desc("monitor file"), llvm::cl::Positional, llvm::cl::init("-"));
+
+static llvm::cl::opt<BuilderKinds>
+        BuilderKind("builder",
+                    llvm::cl::desc("Expression builder:"),
+                    llvm::cl::init(DefaultBuilder),
+                    llvm::cl::values(
+                            clEnumValN(DefaultBuilder, "default",
+                                       "Default expression construction."),
+                            clEnumValN(ConstantFoldingBuilder, "constant-folding",
+                                       "Fold constant expressions."),
+                            clEnumValN(SimplifyingBuilder, "simplify",
+                                       "Fold constants and simplify expressions.")KLEE_LLVM_CL_VAL_END));
+
+void writeResult(std::vector<std::vector<unsigned char> > &result, QueryCommand *QC, const char *filename) {
+
+    for (unsigned i = 0, e = result.size(); i != e; ++i) {
+        std::string outfile = std::string(filename) + "_" + QC->Objects[i]->name + "_replay";
+        std::ofstream out;
+        out.open(outfile, std::ios::out);
+        for (unsigned j = 0; j != QC->Objects[i]->size; ++j) {
+            out << (char)result[i][j];
+        }
+        out.close();
+    }
+}
+
+class Solution {
+private:
+    Solver *solver;
+    Solver *coreSolver;
+    ExprBuilder *builder;
+    Parser *parser = NULL;
+    BuilderKinds kinds;
+
+public:
+    Solution(BuilderKinds kind);
+    bool parse(std::string Filename, MemoryBuffer *MB, std::vector<Decl *> &decls);
+    void computeInitialValue(std::vector<Decl *> decls, std::string Filename);
+};
+
+Solution::Solution(BuilderKinds kind) {
+    kinds = kind;
+    coreSolver = klee::createCoreSolver(CoreSolverToUse);
+    solver = constructSolverChain(coreSolver, "", "", "", "");
+//    parser = Parser::Create(NULL, NULL, NULL, false);
+    switch (kinds) {
+        case DefaultBuilder:
+            builder = createDefaultExprBuilder();
+            break;
+        case ConstantFoldingBuilder:
+            builder = createDefaultExprBuilder();
+            builder = createConstantFoldingExprBuilder(builder);
+            break;
+        case SimplifyingBuilder:
+            builder = createDefaultExprBuilder();
+            builder = createConstantFoldingExprBuilder(builder);
+            builder = createSimplifyingExprBuilder(builder);
+            break;
+    }
+}
+
+bool Solution::parse(std::string Filename, MemoryBuffer *MB, std::vector<Decl *> &decls) {
+    if (parser == NULL) {
+        parser = Parser::Create(Filename, MB, builder, false);
+    }
+    else {
+        parser->updateParserImpl(Filename, MB, false);
+    }
+//    parser = Parser::Create(Filename, MB, builder, false);
+//    parser->update(Filename, MB, builder);
+//    std::vector<Decl *> Decls;
+
+    parser->SetMaxErrors(20);
+    while (Decl *D = parser->ParseTopLevelDecl()) {
+        decls.push_back(D);
+    }
+
+    return true;
+}
+
+
+void Solution::computeInitialValue(std::vector<Decl *> decls, std::string Filename) {
+    for (std::vector<Decl *>::iterator it = decls.begin(),
+                 ie = decls.end(); it != ie; ++it) {
+        Decl *D = *it;
+
+        if (QueryCommand *QC = dyn_cast<QueryCommand>(D)) {
+//            std::cout << solver->getConstraintLog(Query(ConstraintManager(QC->Constraints), QC->Query)) << std::endl;
+            std::vector<std::vector<unsigned char> > result;
+            if (solver->getInitialValues(Query(ConstraintManager(QC->Constraints), QC->Query),
+                                         QC->Objects, result)) {
+                writeResult(result, QC, Filename.c_str());
+            }
+        }
+    }
+            if (uint64_t queries = *theStatisticManager->getStatisticByName("Queries")) {
+            llvm::outs()
+                    << "--\n"
+                    << "total queries = " << queries << "\n"
+                    << "total queries constructs = "
+                    << *theStatisticManager->getStatisticByName("QueriesConstructs") << "\n"
+                    << "valid queries = "
+                    << *theStatisticManager->getStatisticByName("QueriesValid") << "\n"
+                    << "invalid queries = "
+                    << *theStatisticManager->getStatisticByName("QueriesInvalid") << "\n"
+                    << "query cex = "
+                    << *theStatisticManager->getStatisticByName("QueriesCEX") << "\n"
+                    << "query cex cache hits = "
+                    << *theStatisticManager->getStatisticByName("QueryCexCacheHits") << "\n";
+        }
+        llvm::outs() << "\n";
+//    delete(parser);
+
+
+}
+
+std::string getFilename(std::ifstream &in) {
+    std::string res;
+    std::string lastLine;
+    in.clear();
+    in.seekg(0);
+    if (in.is_open()) {
+        while (getline(in, lastLine)){
+            res = lastLine;
+        }
+    }
+    return res;
+}
+
+int main(int argc, char **argv) {
+    llvm::cl::ParseCommandLineOptions(argc, argv);
+    std::cout << "Hello World!" << std::endl;
+
+
+    int fdnotify = -1;
+    fdnotify = inotify_init();
+
+    if (fdnotify < 0) {
+        perror("inofity_init failed");
+        exit(1);
+    }
+
+    int wd = inotify_add_watch(fdnotify, InputFile2.getValue().c_str(), IN_MODIFY);
+    if (wd < 0) {
+        perror("inotify_add_watch failed");
+        exit(1);
+    }
+
+    std::ifstream in(InputFile2.c_str());
+    Solution *solution = new Solution(BuilderKind.getValue());
+
+    while (1) {
+        int len, i = 0;
+        std::string InputFile;
+        char buffer[EVENT_BUF_LEN];
+        len = read(fdnotify, buffer, EVENT_SIZE);
+        if (len < 0) {
+            perror("read");
+        }
+        while ( i < len ) {
+            struct inotify_event *event = ( struct inotify_event * ) &buffer[ i ];
+            if( event->mask & IN_MODIFY) {
+                InputFile = getFilename(in);
+                std::cout << InputFile << std::endl;
+            }
+            i += EVENT_SIZE + sizeof(buffer);
+        }
+
+
+        auto MBResult = MemoryBuffer::getFile(InputFile.c_str());
+        if (!MBResult) {
+            llvm::errs() << InputFile.c_str() << ": error: " << MBResult.getError().message()
+                         << "\n";
+            return 1;
+        }
+        std::unique_ptr<MemoryBuffer> &MB = *MBResult;
+
+        std::vector<Decl *> decls;
+        solution->parse(InputFile, MB.get(), decls);
+        solution->computeInitialValue(decls, InputFile);
+
+        InputFile.clear();
+//        llvm::llvm_shutdown();
+    }
+}
